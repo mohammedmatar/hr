c8bc59ace63b756eeee71da8279dc63d39436569
Mohammed Matar <mohammedmatar@yandex.com>
2016-02-16 16:27:28 +0300

    Update services.js

--- www/js/services.js
+++ www/js/services.js
@@ -2,22 +2,22 @@ angular.module('app.services', ['ngResource'])
 
 .factory('SalaryService', ['$resource', function($resource) {
 	// localStorage.setItem("empId", 1);
-	return $resource('http://192.168.12.1:3000/api/salary/:emp_id', {emp_id: localStorage.getItem("empId")});
+	return $resource('http://192.168.12.1:8080/api/salary/:emp_id', {emp_id: localStorage.getItem("empId")});
   }])
 .factory('AdvancesService', ['$resource', function($resource) {
-	return $resource('http://192.168.12.1:3000/api/advances', {}, {
+	return $resource('http://192.168.12.1:8080/api/advances', {}, {
 		query: {method: 'GET', isArray: true},
 		create: {method: 'POST'}
 	});
   }])
 .factory('VacationsService', ['$resource', function($resource) {
-	return $resource('http://192.168.12.1:3000/api/vacations', {}, {
+	return $resource('http://192.168.12.1:8080/api/vacations', {}, {
 		query: {method: 'GET', isArray: true},
 		create: {method: 'POST'}
 	});
   }])
 .factory('UsersService', ['$resource', function($resource) {
-	return $resource('http://192.168.12.1:3000/api/users', {}, {
+	return $resource('http://192.168.12.1:8080/api/users', {}, {
 		query: {method: 'GET', isArray: true},
 		create: {method: 'POST'}
 	});
